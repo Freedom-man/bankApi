9e7ac08cb23358e53a323a653faf25c6d605a3b5
Freedom-man <zhekamavrin@mail.ru>
2024-01-30 22:33:50 +0300

    Ковыряюсь с коммитами и ветками

--- src/test/java/ru/mavrin/bankapi/DemoApplicationTests.java
+++ src/test/java/ru/mavrin/bankapi/DemoApplicationTests.java
@@ -6,6 +6,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class DemoApplicationTests {
 // нужно написать тесты
+	// но мне впадлу
 	@Test
 	void contextLoads() {
 	}
